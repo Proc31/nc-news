b10525b734bd531a1409e77cb48a37d10bd7ed4e
Matthew Proctor <matt@Matts-Mac.local>
2023-10-04 09:31:35 +0100

    implemented comment_count in GET/api/articles/article_id

--- __tests__/nc_news.test.js
+++ __tests__/nc_news.test.js
@@ -152,22 +152,26 @@ describe('GET /api/articles/:article_id', () => {
 			return request(app)
 				.get('/api/articles/1')
 				.then(({ body }) => {
-					const article = body.article[0];
-					expect(typeof article.article_id).toBe('number');
-					expect(typeof article.title).toBe('string');
-					expect(typeof article.topic).toBe('string');
-					expect(typeof article.author).toBe('string');
-					expect(typeof article.body).toBe('string');
-					expect(typeof article.created_at).toBe('string');
-					expect(typeof article.votes).toBe('number');
-					expect(typeof article.article_img_url).toBe('string');
+					const article = body.article;
+					const articleFormat = {
+						author: expect.any(String),
+						title: expect.any(String),
+						article_id: expect.any(Number),
+						body: expect.any(String),
+						topic: expect.any(String),
+						created_at: expect.any(String),
+						votes: expect.any(Number),
+						article_img_url: expect.any(String),
+						comment_count: expect.any(Number),
+					};
+					expect(article).toMatchObject(articleFormat);
 				});
 		});
 		test('GET:200 expects response to have correct content', () => {
 			return request(app)
 				.get('/api/articles/1')
 				.then(({ body }) => {
-					const article = body.article[0];
+					const article = body.article;
 					const articleOne = {
 						article_id: 1,
 						title: 'Living in the shadow of a great man',
@@ -176,6 +180,7 @@ describe('GET /api/articles/:article_id', () => {
 						body: 'I find this existence challenging',
 						created_at: '2020-07-09T20:11:00.000Z',
 						votes: 100,
+						comment_count: 11,
 						article_img_url:
 							'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700',
 					};
@@ -212,7 +217,7 @@ describe('GET /api/articles/:article_id/comments', () => {
 			return request(app)
 				.get('/api/articles/1/comments')
 				.then(({ body }) => {
-					const obj = {
+					const articleFormat = {
 						comment_id: expect.any(Number),
 						votes: expect.any(Number),
 						created_at: expect.any(String),
@@ -223,7 +228,7 @@ describe('GET /api/articles/:article_id/comments', () => {
 					const comments = body.comments;
 					expect(comments).toHaveLength(11);
 					comments.forEach((comment) => {
-						expect(comment).toMatchObject(obj);
+						expect(comment).toMatchObject(articleFormat);
 					});
 				});
 		});
